4c4374f828a86c9696ce740dc64d7002bf4fcebd
TheHurricaneCat <thc12895nth@gmail.com>
2023-11-09 16:47:39 +0800

    Givy language-based dynamic GUI

--- Python/testingGrounds.py
+++ Python/testingGrounds.py
@@ -1,27 +1,66 @@
+import threading
+import sqlite3 as lite
 import kivy 
 from kivy.app import App 
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
-class TestGrid(GridLayout):
+
+class ProductSelection(BoxLayout):
+    pass
+
+class TestGrid(Widget):
     def __init__(self, **kwargs):
         super(TestGrid, self).__init__(**kwargs)
-         
+        
+        for num in range(1, 13):
+            productButton = ProductSelection()
+            productButton.ids.label1.text = str(num)
+            self.ids.container.add_widget(productButton)
+
+        #productWidget = ProductSelection()
+        #self.ids.container.add_widget(productWidget)
+        
+class TestApp(App):
+    def build(self):
+        return TestGrid()
+    
+if __name__ == '__main__':
+    TestApp().run()
+
+    """
         #Main Grid
         self.cols = 3
+        
         #Generate the title
-        self.add_widget(Label(text="PRODUCT"))
-        self.add_widget(Label(text="KEYPAD"))
-        self.add_widget(Label(text="OVERVIEW"))
+        self.add_widget(Label(text="PRODUCT", 
+            size_hint_y = None,
+            height=100,
+            ))
+        self.add_widget(Label(text="KEYPAD", 
+            size_hint_y = None,
+            height=100
+            ))
+        self.add_widget(Label(text="OVERVIEW", 
+            size_hint_y = None,
+            height=100
+            ))
         
         #Product Slice
         self.prodGrid = GridLayout()
         self.prodGrid.cols = 3
 
         for num in range(1, 13):
-            self.submit = Button(text="Product " + str(num))
+            self.submit = Button(text="Product " + str(num),
+                size_hint_y = None, 
+                height = 100,
+                size_hint_x = None, 
+                width = 100
+                )
             self.prodGrid.add_widget(self.submit)
         self.add_widget(self.prodGrid)
         
@@ -30,13 +69,18 @@ class TestGrid(GridLayout):
         self.keyGrid = GridLayout()
         self.keyGrid.cols = 3
         
-        keys = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+        keys = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "ENTER", "DEL"]
         for num in keys:
-            self.submit = Button(text=str(num))
+            self.submit = Button(text=str(num),
+                size_hint_y = None, 
+                height = 100,
+                size_hint_x = None, 
+                width = 100
+                )
             self.keyGrid.add_widget(self.submit)
-        self.submit = Button(text="ENTER")
-        self.keyGrid.add_widget(self.submit)
+        
         self.add_widget(self.keyGrid)
+       
 
         #Display and Enter Slice
         self.dispGrid = GridLayout()
@@ -47,22 +91,4 @@ class TestGrid(GridLayout):
         self.submit = Button(text="ENTER")
         self.dispGrid.add_widget(self.submit)
         self.add_widget(self.dispGrid)
-
         """
-        #Title
-        self.add_widget(Label(text="Staff"))
-
-        #Text input
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-        #Button
-        self.submit = Button(text="Submit")
-        self.add_widget(self.submit)
-        """
-
-class TestApp(App):
-    def build(self):
-        return TestGrid()
-    
-if __name__ == '__main__':
-    TestApp().run()
