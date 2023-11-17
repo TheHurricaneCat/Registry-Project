1feeafe53230816cfe39fab96e18e932c114b53e
TheHurricaneCat <thc12895nth@gmail.com>
2023-11-18 01:09:47 +0800

    Integration of the excel processes & error boxes

--- Python/testingGrounds.py
+++ Python/testingGrounds.py
@@ -14,11 +14,19 @@ from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 
-global prodInfo
-prodInfo = [""]
+import random
 
-global defaultMsg
-defaultMsg = "ENTER THE QUANTITY"
+import openpyxl
+from datetime import date
+
+global prodInfo
+prodInfo = ["Staff Name"]
+
+global defaultNumMsg
+defaultNumMsg = "ENTER THE QUANTITY"
+
+global defaultProdMsg
+defaultProdMsg = "ENTER THE PRODUCT"
 
 global staffFile
 staffFile = "C:/Users/Josefe Gillego/Documents/Special Project/Python/staff.txt"
@@ -38,7 +46,7 @@ class ProdScroll(BoxLayout):
 
 class ProdSelection(BoxLayout):
     global curProd
-    curProd = defaultMsg
+    curProd = defaultProdMsg
     def onProdPress(self):
         global curProd
         curProd = self.ids.label1.text
@@ -53,28 +61,42 @@ class KeyMod(Label):
 
 class KeyPad(BoxLayout):
     global curNum
-    curNum = defaultMsg
+    curNum = defaultNumMsg
     def onKeyPress(self):
         global curNum
         global curProd
         global prodInfo
         if self.ids.button2.text == "DEL":
-            curNum = defaultMsg
+            curNum = defaultNumMsg
             updateKeys()
             print("Status: deleting quantity")
         elif self.ids.button2.text == "ENTER":
-            if (curNum != defaultMsg and curProd != defaultMsg and int(curNum)*2 != 0):
+            if (curNum != defaultNumMsg and curProd != defaultProdMsg and int(curNum)*2 != 0):
                 prodInfo = prodInfo + [str(curProd)]
                 prodInfo = prodInfo + [str(curNum)]
                 updateView()
-                curNum = defaultMsg
-                curProd = defaultMsg
+                curNum = defaultNumMsg
+                curProd = defaultProdMsg
                 updateKeys()
                 updateProd()
             else: 
-                print("Error: quantity is zero or not stated ")
+                if (curNum == defaultNumMsg and curProd == defaultProdMsg): 
+                    error = StaffError()
+                    error.open()
+                    error.title = "Error: No product or quantity chosen"
+                    print("Error: quantity is zero or not stated ")
+                elif (curNum == defaultNumMsg or int(curNum)*2 == 0): 
+                    error = StaffError()
+                    error.open()
+                    error.title = "Error: No quantity entered or is zero"
+                    print("Error: quantity is zero")
+                elif (curProd == defaultProdMsg): 
+                    error = StaffError()
+                    error.open()
+                    error.title = "Error: No product chosen"
+                    print("Error: product is not stated ")
         else:
-            if (curNum == "Default" or curNum == defaultMsg):
+            if (curNum == "Default" or curNum == defaultNumMsg):
                 curNum = ""
             curNum = curNum + str(self.ids.button2.text)
             updateKeys()
@@ -94,7 +116,13 @@ class ViewPad(BoxLayout):
         delElem()
         print(self.ids.button3.text)
     def onEnterPress(self):
-        print(self.ids.button4.text)
+        if (currStaff != ""):
+            processData()
+            print(self.ids.button4.text)
+        else:
+            error = StaffError()
+            error.open()
+            error.title = "Error: No staff selected"
 
 class ViewSelection(BoxLayout):
     global index 
@@ -115,16 +143,50 @@ class StaffPad(BoxLayout):
         updateCurrentStaff()
         
 class StaffSelection(BoxLayout):
-    pass
-
+    global currStaff
+    currStaff = ""
+    def updateStaff(self):
+        global currStaff
+        currStaff = self.ids.staffTitle.text
+        staffEntry.ids.currentStaff.text = currStaff
+        confirmation = StaffConfirm()
+        confirmation.open()
+        confirmation.title = "Set <" + currStaff + "> as the current staff?"
+        
 class StaffConfirmation(Popup):
+    def __init__(self, **kwargs):
+        super(StaffConfirmation, self).__init__(**kwargs)
+        for person in staffList:
+            staffEntry = StaffSelection()
+            staffEntry.ids.staffTitle.text = str(person)
+            self.ids.staffContainer.add_widget(staffEntry)
     def confirmStaff(self):
-        updateStaff()
-        self.dismiss()
-    pass
+        staffEntry = StaffSelection()
+        global currInput
+        currInput = self.ids.textInput.text
+        if (currInput != ""):
+            staffEntry.ids.staffTitle.text = currInput
+            self.ids.staffContainer.add_widget(staffEntry)
+            self.ids.textInput.text = currInput + " is successully added!"
+            f = open(staffFile,"a+")
+            f.write("\n")
+            f.write(currInput)
+            f.close()
+        else:
+            error = StaffError()
+            error.open()
+            error.title = "Error: No input"
+    def clearField(self):
+        self.ids.textInput.text = ""
 
 class StaffError(Popup):
     pass
+
+class StaffConfirm(Popup):
+    def setStaff(self):
+        self.dismiss()
+        staffEntry.dismiss()
+        updateCurrentStaff()
 
 #Main Grid
 class TestGrid(Widget):
@@ -172,25 +234,14 @@ class TestGrid(Widget):
         
         confirmButton = ViewPad()
         addConfirm.ids.viewContainer.add_widget(confirmButton)
-        
-        #Generate the staff panel
-        addStaffPad = StaffPad()
-        self.ids.staffGrid.add_widget(addStaffPad)
-          
-        for person in staffList:
-            staffEntry = StaffSelection()
-            staffEntry.ids.staffTitle.text = str(person)
-            self.ids.staffContainer.add_widget(staffEntry)
 
-        
         #Update changes
         global updateProd
         global updateKeys
         global updateView
-        global updateStaff
         global updateCurrentStaff
-        global updateStaff
         global delElem
+        global processData
         def updateProd():
             prodTitle.text = str(curProd)
         def updateKeys():
@@ -204,25 +255,80 @@ class TestGrid(Widget):
             addViewScroll.ids.viewScrollContainer.add_widget(addProductView)
             index = index+2
             print(prodInfo)
-        def updateStaff():
-            staffEntry = StaffSelection()
-            staffEntry.ids.staffTitle.text = str(currInput)
-            self.ids.staffContainer.add_widget(staffEntry)
-        def updateCurrentStaff():
-            global currInput
-            currInput = addStaffPad.ids.textInput.text
-            staffPopup = StaffConfirmation()
-            if (currInput != ""):
-                staffPopup.title = "Are you sure you want to add this person? " + "[" + currInput + "]"
-                staffPopup.open()
-            else: 
-                StaffError().open()
-
+        def updateCurrentStaff(): 
+            print("updating")
+            self.ids.staffTitle.text = "Current Staff: " + currStaff
+            prodInfo[0] = currStaff
+        def processData():
+            for space in prodInfo:
+                if (space == ""):
+                    del prodInfo[space]
+            excelProcess()
+            delElem()
         def delElem():
             row = [i for i in addViewScroll.ids.viewScrollContainer.children]
             for child in row:
                 addViewScroll.ids.viewScrollContainer.remove_widget(child)
+            prodInfo[1:] = ""
+            global index
+            index = 1
+            print(prodInfo)
+        def excelProcess():
+            wb = openpyxl.load_workbook(r"C:/Users/Josefe Gillego/Documents/Special Project/Python/TestFile.xlsx")
+            ws = wb["Sheet1"]
 
+            # Get the column length and ignore whitespaced cells
+            currentLocation = 1
+            for i in range(1, ws.max_row):
+                if ws["A" + str(i)].value != None:
+                    print(currentLocation)
+                    currentLocation += 1
+
+            # Write data in excel
+            colNames = ["A", "B", "C", "D", "E"]
+
+            # Initialize invoice number
+            invNum = str(ws["D" + str(currentLocation - 1)].value)
+            print(invNum)
+            print(prodInfo)
+
+            for column in range(5):
+                # writing the date, staff and invoice number
+                def writeExcel(identifier, data):
+                    ws[str(colNames[column]) + str(currentLocation + item - identifier)] = data
+                
+                for item in range(1, int((len(prodInfo)) / 2) + 1):
+                    if (column == 0): 
+                        writeExcel(1, date.today())
+                    if (column == 3): 
+                        multi = False
+                        if (multi == False):
+                            writeExcel(1, int(date.today().strftime('%y%m%d') + format(int(invNum[6:]) + 1, '004d')))
+                            multi = True
+                        else:
+                            writeExcel(1, int(ws[str(colNames[column]) + str(currentLocation - 1)]))
+                    if (column == 4):
+                            writeExcel(1, prodInfo[0])
+                # write the product code and quantity
+                identA = 1
+                identB = 2
+                for item in range(1, len(prodInfo)):
+                    if (column == 1 and item%2 != 0):
+                        writeExcel(identA, prodInfo[item])
+                        identA += 1
+                    if (column == 2 and item%2 == 0): 
+                        writeExcel(identB, int(prodInfo[item]))
+                        identB += 1
+
+            wb.save(r"C:/Users/Josefe Gillego/Documents/Special Project/Python/TestFile.xlsx")
+            print("excel written")
+
+    def activatePopup(self):
+        global staffEntry
+        staffEntry = StaffConfirmation()
+        staffEntry.open()
+        self.ids.staffTitle.background_color = random.uniform(0.5, 0.8), random.uniform(0.5, 0.8), random.uniform(0.5, 0.8), 1
+    
 class TestApp(App):
     def build(self):
         Window.size = 1100, (4*120)
